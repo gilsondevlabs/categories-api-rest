d4139ea6cd515dec0708fae68f7201d8939f1023
gilsondev <me@gilsondev.in>
2016-06-09 11:41:07 -0300

    Create custom field to recursive fields

--- categories/core/serializers.py
+++ categories/core/serializers.py
@@ -5,8 +5,20 @@ from rest_framework import serializers
 from categories.core.models import Category
 
 
+class RecursiveField(serializers.Serializer):
+    """
+    Cria instancia do serializer parente e retorna os dados
+    serializados.
+    """
+    def to_representation(self, value):
+        ParentSerializer = self.parent.parent.__class__
+        serializer = ParentSerializer(value, context=self.context)
+        return serializer.data
+
+
 class CategorySerializer(serializers.ModelSerializer):
+    subcategories = RecursiveField(source="children", many=True)
+
     class Meta:
         model = Category
-        fields = ("id", "name", "children",)
-        depth = 1
+        fields = ("id", "name", "subcategories",)
